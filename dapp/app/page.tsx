4f141ed8ad80dd2eaf95d853f1b6f80c177073f4
renzocabarios <renzo.cabarios@gmail.com>
2024-08-31 20:41:46 +0800

    Update

--- dapp/app/page.tsx
+++ dapp/app/page.tsx
@@ -2,10 +2,17 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import Image from "next/image";
 import {
   IVaultSchema,
   VaultSchema,
@@ -23,15 +30,30 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex flex-col gap-4">
-        <Input></Input>
+    <Form {...form}>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <div className="flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="amount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Amount</FormLabel>
+                <FormControl>
+                  <Input placeholder="enter amount" {...field} />
+                </FormControl>
+                <FormDescription>Enter valid amount</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <div className="flex items-center justify-between gap-4 w-full">
-          <Button>Deposit</Button>
-          <Button>Withdraw</Button>
+          <div className="flex items-center justify-between gap-4 w-full">
+            <Button onClick={form.handleSubmit(onSubmit)}>Deposit</Button>
+            <Button onClick={form.handleSubmit(onSubmit)}>Withdraw</Button>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </Form>
   );
 }
