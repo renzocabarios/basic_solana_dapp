8eac8a919a8eba688d4c70b81ad601f1bccbe591
renzocabarios <renzo.cabarios@gmail.com>
2024-08-31 21:05:10 +0800

    Update anchor_vault

--- anchor_vault/programs/anchor_vault/src/lib.rs
+++ anchor_vault/programs/anchor_vault/src/lib.rs
@@ -6,11 +6,22 @@ declare_id!("34ACvqSMwT1fvpzgE9WfxxyQ8guZcZnz18qduJaqTnwJ");
 pub mod anchor_vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn deposit(ctx: Context<Vault>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+
+    pub fn wtihdraw(ctx: Context<Vault>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Vault<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut, seeds = [signer.key().as_ref()], bump)]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
